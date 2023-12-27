d53c6b8993aef1a25ec6ff4491c8001e4a444da4
Isidro Butaslac <butaslaciii@gmail.com>
2023-12-27 11:32:02 +0800

    linear regression

---
+++ Assets/Scripts/LogDistModel/DistanceCalculator.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class DistanceCalculator : MonoBehaviour
+{
+    // Calibration constants for each device
+    public float[] referenceRSSI; // RSSI at the reference distance (1 meter)
+    public float[] pathLossExponent; // Path loss exponent for each device
+
+    // Function to calculate distance based on RSSI
+    public float CalculateDistance(int deviceID, float rssi)
+    {
+        if (deviceID < 0 || deviceID >= referenceRSSI.Length)
+        {
+            Debug.LogError("Invalid device ID");
+            return -1f;
+        }
+
+        // Using the Log-Distance Path Loss Model
+        float distance = Mathf.Pow(10, ((referenceRSSI[deviceID] - rssi) / (10 * pathLossExponent[deviceID])));
+
+        return distance;
+    }
+
+    // Example usage
+    void Start()
+    {
+        // Example: Calculate distance for Device 0 with RSSI value -70.5
+        int deviceID = 0;
+        float rssiValue = -70.5f;
+        float distance = CalculateDistance(deviceID, rssiValue);
+
+        Debug.Log($"Distance from Device {deviceID}: {distance} meters");
+    }
+}
---
+++ Assets/Scripts/LogDistModel/PathLossCalibrator.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+public class PathLossCalibrator : MonoBehaviour
+{
+    // Calibration constants for each device
+    public float[] referenceRSSI; // RSSI at the reference distance (1 meter)
+    public float[] pathLossExponent; // Path loss exponent for each device
+
+    // Function to calibrate the path loss exponent
+    public void CalibratePathLossExponent(int deviceID, float[] distances, float[] rssiValues)
+    {
+        if (deviceID < 0 || deviceID >= referenceRSSI.Length)
+        {
+            Debug.LogError("Invalid device ID");
+            return;
+        }
+
+        // Check if there are enough samples
+        if (distances.Length != rssiValues.Length || distances.Length < 2)
+        {
+            Debug.LogError("Insufficient data for calibration");
+            return;
+        }
+
+        // Use linear regression to estimate path loss exponent
+        float sumX = 0f;
+        float sumY = 0f;
+        float sumXY = 0f;
+        float sumX2 = 0f;
+
+        for (int i = 0; i < distances.Length; i++)
+        {
+            float x = 10 * Mathf.Log10(distances[i]);
+            float y = referenceRSSI[deviceID] - rssiValues[i];
+
+            sumX += x;
+            sumY += y;
+            sumXY += x * y;
+            sumX2 += x * x;
+        }
+
+        float n = (distances.Length * sumXY - sumX * sumY) / (distances.Length * sumX2 - sumX * sumX);
+
+        // Update path loss exponent for the device
+        pathLossExponent[deviceID] = n;
+
+        Debug.Log($"Calibrated Path Loss Exponent for Device {deviceID}: {pathLossExponent[deviceID]}");
+    }
+
+    void Start()
+    {
+        int deviceID = 0;
+        float[] distances = { 1f, 2f, 3f, 4f, 5f };
+        float[] rssiValues = { -50f, -60f, -70f, -80f, -90f };
+
+        CalibratePathLossExponent(deviceID, distances, rssiValues);
+    }
+}
